2521080ab8b523b94fe21ac07cf00b74ac2bc5ec
danildperez04 <gamerdan589@gmail.com>
2023-10-30 13:43:38 -0600

    add(models): client model

---
+++ backend/src/models/client.js
@@ -0,0 +1,16 @@
+// const { DataTypes, Model } = require('sequelize');
+// const sequelize = require('../db');
+
+// class Client extends Model {}
+
+// Client.init({
+//   firstName,
+//   lastName,
+//   genre,
+// },
+// {
+//   sequelize, 
+//   modelName: 'client'
+// });
+
+// module.exports = { Client };
