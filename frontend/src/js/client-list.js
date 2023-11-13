7085032f5449f045831ce9f5505d9521f8baebdc
danildperez04 <gamerdan589@gmail.com>
2023-11-13 13:37:53 -0600

    update(frontend): formatting

--- frontend/src/js/client-list.js
+++ frontend/src/js/client-list.js
@@ -1,38 +1,38 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    start();
+document.addEventListener('DOMContentLoaded', () => {
+  start();
 });
 
 
-function start(){
-    fetch('http://localhost:3000/api/clients')
+function start() {
+  fetch('http://localhost:3000/api/clients')
     .then(response => response.json())
     .then(data => showClients(data));
 }
 
-function showClients(data){
-    const table = document.querySelector('.client-list tbody');
+function showClients(data) {
+  const table = document.querySelector('.client-list tbody');
 
-    data.forEach(client => {
-        const row = document.createElement('tr');
-        row.classList.add('row');
-        Object.keys(client).forEach(key =>{
-            if(key !== 'id' && key !== 'createdAt' && key !== 'updatedAt'){
-                const cell = document.createElement('td');
-                cell.textContent = client[key];
-                row.appendChild(cell);
-            }
-        });
-        const actions = document.createElement('td');
-        actions.classList.add('actions');
-        btnUpdate = document.createElement('a');
-        btnDelete = document.createElement('a');
-        btnUpdate.textContent = 'Actualizar';
-        btnUpdate.classList.add('btn-update');
-        btnDelete.textContent = 'Eliminar';
-        actions.appendChild(btnUpdate);
-        actions.appendChild(btnDelete);
-        row.appendChild(actions);
-        table.appendChild(row);
+  data.forEach(client => {
+    const row = document.createElement('tr');
+    row.classList.add('row');
+    Object.keys(client).forEach(key => {
+      if (key !== 'id' && key !== 'createdAt' && key !== 'updatedAt') {
+        const cell = document.createElement('td');
+        cell.textContent = client[key];
+        row.appendChild(cell);
+      }
     });
+    const actions = document.createElement('td');
+    actions.classList.add('actions');
+    btnUpdate = document.createElement('a');
+    btnDelete = document.createElement('a');
+    btnUpdate.textContent = 'Actualizar';
+    btnUpdate.classList.add('btn-update');
+    btnDelete.textContent = 'Eliminar';
+    actions.appendChild(btnUpdate);
+    actions.appendChild(btnDelete);
+    row.appendChild(actions);
+    table.appendChild(row);
+  });
 
 }
--- frontend/src/js/client.js
+++ frontend/src/js/client.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    startApp();
+document.addEventListener('DOMContentLoaded', () => {
+  startApp();
 });
 
-function startApp(){
-    const form = document.querySelector('.client-form');
-    form.addEventListener('submit', (e)=>{
-        e.preventDefault();
-        const clientData = Object.fromEntries(new FormData(e.target));
+function startApp() {
+  const form = document.querySelector('.client-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const clientData = Object.fromEntries(new FormData(e.target));
 
-        fetch('http://localhost:3000/api/clients', {
-            method: 'POST',
-            body: JSON.stringify(clientData),
-            headers: {
-                'Content-Type': 'application/json' 
-            }
-        })
-        .then(response => {
-            if(response.ok)
-            console.log('Se creo el cliente');
-        });
-    });
+    fetch('http://localhost:3000/api/clients', {
+      method: 'POST',
+      body: JSON.stringify(clientData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.ok)
+          console.log('Se creo el cliente');
+      });
+  });
 }
