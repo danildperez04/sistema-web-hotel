53f9ef707e0c15746fe4aaf7a3f5bc83fd6f8407
danildperez04 <gamerdan589@gmail.com>
2023-11-18 15:31:25 -0600

    update(controllers): formatting

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,45 +1,52 @@
 const User = require('../services/users.service');
 const userService = new User();
 
-const getAll = async (req, res)=>{  
+const getAll = async (req, res) => {
   const users = await userService.getAll();
-  
+
   res.send(users);
 };
 
-const getOne = async (req, res)=>{  
-  const {id} = req.params;
-  res.send(`Get User {${id}}`);
+const getOne = async (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = await userService.getOne(id);
+
+  if (!foundUser) {
+    return res.status(404).send({
+      message: 'User not found',
+    });
+  }
+
+  res.send(foundUser);
 };
 
-const create = async (req, res)=>{  
-  const {firstName, lastName, email, username, password} = req.body;
+const create = async (req, res) => {
+  const { firstName, lastName, email, username, password } = req.body;
 
   const user = await userService.create({
     firstName,
     lastName,
     email,
-    username, 
+    username,
     password
   });
 
   res.send(user);
 };
 
-const update = async (req, res)=>{  
-  const {id} = req.params;
-  res.send(`Update User {${id}}`);
+const update = async (req, res) => {
+
 };
 
-const remove = async (req, res)=>{  
-  const {id} = req.params;
-  res.send(`Remove User {${id}}`);
+const remove = async (req, res) => {
+
 };
 
 module.exports = {
-  getAll, 
+  getAll,
   getOne,
-  create, 
-  update, 
+  create,
+  update,
   remove
 };
