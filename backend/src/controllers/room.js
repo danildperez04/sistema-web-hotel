4cbe446131550e182f54082e6f4b92a664457b48
Eduardo Cruz <eduardolenincruzlopez0@gmail.com>
2023-11-18 20:47:04 -0600

    room controllers

--- backend/src/controllers/room.js
+++ backend/src/controllers/room.js
@@ -1,21 +1,72 @@
-const getAll = async (req, res) => {
+const Room = require('../services/room.service');
+const roomServices = new Room();
 
+const getAll = async (req, res) => {
+  const rooms = await roomServices.getAll();
+
+  res.send(rooms);
 };
 
 const getOne = async (req, res) => {
+  const { id } = req.params;
 
+  const foundRoom = await roomServices.getOne(id);
+
+  if (!foundRoom) {
+    return res.status(404).send({
+      message: 'Room not found'
+    });
+  }
+
+  res.send(foundRoom);
 };
 
 const create = async (req, res) => {
+  const {code, price, description} = req.body;
+  const room = new roomServices.create({
+    code,
+    price,
+    description,
+  });
 
+  res.send(room);
 };
 
 const update = async (req, res) => {
+  const { id } = req.params;
+  const {code, price, description} = req.body;
+  const foundRoom = await roomServices.getOne(id);
 
+  if (!foundRoom) {
+    return res.status(404).send({
+      message: 'Room not found'
+    });
+  }
+
+  const roomToUpdate = {
+    code,
+    price,
+    description,
+  };
+
+  const updatedRoom = await roomServices.update(roomToUpdate, id);
+
+  res.send(updatedRoom);
 };
 
 const remove = async (req, res) => {
+  const { id } = req.params;
+  const foundRoom = await roomServices.getOne(id);
 
+  if (!foundRoom) {
+    return res.status(404).send({
+      message: 'Room not found'
+    });
+  }
+
+  await roomServices.remove(id);
+
+  res.sendStatus(204);
 };
 
 module.exports = {
