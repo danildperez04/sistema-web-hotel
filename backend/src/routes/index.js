e751d46216900c7f7e0c744f12340a2d06195016
danildperez04 <gamerdan589@gmail.com>
2023-12-05 15:34:01 -0600

    update(routes): roomsRouter

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const usersRouter = require('./users.js');
 const clientsRouter = require('./clients.js');
+const roomsRouter = require('./room.js');
 
 router.post('/auth');
 router.use('/users', usersRouter);
 router.use('/clients', clientsRouter);
+router.use('/rooms', roomsRouter);
 
 module.exports = router; 
