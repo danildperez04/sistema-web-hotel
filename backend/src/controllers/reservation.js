910fc6dccb66d12d3cb51c366dc0bed27987b859
danildperez04 <gamerdan589@gmail.com>
2023-12-07 10:55:49 -0600

    update(routes): reservationsRouter

---
+++ backend/src/controllers/reservation.js
@@ -0,0 +1,29 @@
+//TODO: Reservation Controller
+
+const getAll = () => {
+
+};
+
+const getOne = () => {
+
+};
+
+const create = () => {
+
+};
+
+const update = () => {
+
+};
+
+const remove = () => {
+
+};
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  remove
+};
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,16 @@ const router = require('express').Router();
 
 const usersRouter = require('./users.js');
 const clientsRouter = require('./clients.js');
+const servicesRouter = require('./services.js');
 const roomsRouter = require('./room.js');
+const reservationsRouter = require('./reservation.js');
 
+//TODO: Auth route
 router.post('/auth');
 router.use('/users', usersRouter);
 router.use('/clients', clientsRouter);
+router.use('/services', servicesRouter);
 router.use('/rooms', roomsRouter);
+router.use('/reservations', reservationsRouter);
 
 module.exports = router; 
