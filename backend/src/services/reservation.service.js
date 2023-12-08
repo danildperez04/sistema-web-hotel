62463ce6e456bc89f3f6f0596a8279d2ec78d7ab
danildperez04 <gamerdan589@gmail.com>
2023-12-08 15:34:05 -0600

    update(service): getAll

---
+++ backend/src/services/reservation.service.js
@@ -0,0 +1,15 @@
+const { Reservation: reservationModel } = require('../models/reservation');
+const { Reservation_Room } = require('../models/reservation_room');
+const { Reservation_Service } = require('../models/reservation_service');
+
+class Reservation {
+  async getAll() {
+    const reservations = await reservationModel.findAll({
+      include: [Reservation_Room, Reservation_Service]
+    });
+
+    return reservations;
+  }
+}
+
+module.exports = Reservation;
