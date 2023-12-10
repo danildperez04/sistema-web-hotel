bd900720f7a1530ef745798f49bedbc1308dd2e8
danildperez04 <gamerdan589@gmail.com>
2023-12-09 19:59:53 -0600

    update(http): http requests

--- backend/src/middlewares/not-found.js
+++ backend/src/middlewares/not-found.js
@@ -2,7 +2,7 @@ const { NotFoundException } = require('../utils/customErrors');
 
 // eslint-disable-next-line no-unused-vars
 const notFound = (req, res, _next) => {
-  throw new NotFoundException();
+  throw new NotFoundException(`${req.path} path not found`);
 };
 
 module.exports = {
