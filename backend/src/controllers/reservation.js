a3b3293242103006540327ecb7345bdc167ef072
danildperez04 <gamerdan589@gmail.com>
2023-12-10 14:50:27 -0600

    update(controller) extract user in update reservation

--- backend/src/controllers/reservation.js
+++ backend/src/controllers/reservation.js
@@ -1,9 +1,7 @@
 //TODO: Reservation Controller
 const Reservation = require('../services/reservation.service');
-const User = require('../services/users.service');
 const Client = require('../services/client.service');
 const reservationServices = new Reservation();
-const userService = new User();
 const clientService = new Client();
 
 const getAll = async (req, res) => {
@@ -50,19 +48,15 @@ const create = async (req, res) => {
 
 const update = async (req, res) => {
   const { id } = req.params;
-  const { userId, clientId, startDate, endDate, cancelled, services, rooms } = req.body;
+  const { clientId, startDate, endDate, cancelled, services, rooms } = req.body;
+  const { user } = req;
+
   const foundReservation = await reservationServices.getOne(id);
 
   if (!foundReservation) {
     return res.status(404).send({
       message: 'Reservation not found'
     });
-  }
-
-  const user = await userService.getOneById(userId);
-
-  if (!user) {
-    return res.status(404).send({ message: 'user not found' });
   }
 
   const client = await clientService.getOne(clientId);
@@ -72,17 +66,16 @@ const update = async (req, res) => {
   }
 
   const reservationToUpdate = {
-    userId,
+    userId: user.id,
     clientId,
     startDate,
     endDate,
     cancelled,
   };
 
-
   const reservationUpdated = await reservationServices.update(reservationToUpdate, { services, rooms }, id);
-  res.send(reservationUpdated);
 
+  res.send(reservationUpdated);
 };
 
 const remove = () => {
