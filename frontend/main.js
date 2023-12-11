b4b8603bb5e585bf2bdd81e96bf75c4557848647
Eduardo Cruz <eduardolenincruzlopez0@gmail.com>
2023-12-10 22:20:24 -0600

    report test frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,5 @@
+import { getReport } from "./src/services/report";
+
 document.addEventListener('DOMContentLoaded', () => {
   displayModal();
 });
@@ -16,10 +18,10 @@ function displayModal() {
 
         <form class="form">
             <label>Ingrese la cantidad de reportes que desea generar</label>
-            <input type="number" placeholder="Cantidad de reportes">
+            <input type="number" id="input-limit-report" name="limit" placeholder="Cantidad de reportes">
             <label for="input-date-report">Seleccione la fecha del reporte</label>
-            <input type="date" id="input-date-report">
-        <input type="submit" value="Generar Reporte" class="btn-create-room btn-create">
+            <input type="date" id="input-date-report" name="date">
+        <input type="submit" id="btn-create-report" value="Generar Reporte" class="btn-create-room btn-create">
         </form>
       <div>
       
@@ -38,6 +40,16 @@ function displayModal() {
         .addEventListener('click', () => {
           modal.remove();
         })
+
+        document.querySelector('.form').addEventListener('submit', (e) => {
+          e.preventDefault();
+          
+          const reportData = Object.fromEntries( new FormData(e.target) );
+          
+          getReport(reportData);
+          
+        });
+
     });
 
 
---
+++ frontend/src/services/report.js
@@ -0,0 +1,12 @@
+import { getToken } from "../js/token";
+
+const token = getToken();
+
+export async function getReport( { limit , date } ) {
+  const response = await fetch(`http://localhost:3000/api/reports?limit=${limit}&date=${date}`,{
+    headers: {
+      'authorization': 'bearer ' + token
+    }
+  });
+  return await response.json();
+}
